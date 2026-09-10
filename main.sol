9407d2723c2c548f7d95157c1402b83c53dfb356
justv030-cyber <justv030@gmail.com>
2026-09-11 02:15:58 +0530

    add supply on main.sol

--- main.sol
+++ main.sol
@@ -1,43 +1,43 @@
 
-// SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.7.0
-pragma solidity ^0.8.27;
+    // SPDX-License-Identifier: MIT
+    // Compatible with OpenZeppelin Contracts ^5.7.0
+    pragma solidity ^0.8.27;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+    import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+    import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+    import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-contract STUBA is ERC1155, Ownable, ERC1155Burnable {
-    uint256 public _nextTokenId;
+    contract STUBA is ERC1155, Ownable, ERC1155Burnable {
+        uint256 public _nextTokenId;
 
-    mapping(uint256 => uint256) public totalMinted;
+        mapping(uint256 => uint256) public totalMinted;
 
-    mapping(uint256 => uint256) public maxSupply;
+        mapping(uint256 => uint256) public maxSupply;
 
-    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {
-        _nextTokenId = 1;
+        constructor(address initialOwner, uint256 id , uint256 supply) ERC1155("") Ownable(initialOwner) {
+            maxSupply[id] = supply;
+        }
+
+        function setURI(string memory newuri) public onlyOwner {
+            _setURI(newuri);
+        }
+
+        function mint(
+            address account,
+            uint256 id,
+            uint256 amount,
+            bytes memory data
+        ) public onlyOwner {
+            require(totalMinted[id] + amount <= maxSupply[id], "Exceeds maximum supply");
+            _mint(account, id, amount, data);
+        }
+
+        function mintBatch(
+            address to,
+            uint256[] memory ids,
+            uint256[] memory amounts,
+            bytes memory data
+        ) public onlyOwner {
+            _mintBatch(to, ids, amounts, data);
+        }
     }
-
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
-    function mint(
-        address account,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public onlyOwner {
-        require(totalMinted[id] + amount <= maxSupply[id], "Exceeds maximum supply");
-        _mint(account, id, amount, data);
-    }
-
-    function mintBatch(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyOwner {
-        _mintBatch(to, ids, amounts, data);
-    }
-}
