a9a155942355074be7c87ca377164c4677fecfb7
justv030-cyber <justv030@gmail.com>
2026-09-11 02:10:15 +0530

    add maximum supply adn total mint supply

--- main.sol
+++ main.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.7.0
 pragma solidity ^0.8.27;
@@ -7,23 +8,36 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract STUBA is ERC1155, Ownable, ERC1155Burnable {
-    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+    uint256 public _nextTokenId;
+
+    mapping(uint256 => uint256) public totalMinted;
+
+    mapping(uint256 => uint256) public maxSupply;
+
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {
+        _nextTokenId = 1;
+    }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        require(totalMinted[id] + amount <= maxSupply[id], "Exceeds maximum supply");
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
     }
 }
